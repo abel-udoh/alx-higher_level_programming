eb263bb1834df86e82f802adddef80e7453d45ae
abel-udoh <abeludoh8@gmail.com>
2023-04-22 10:13:12 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,18 +2,11 @@
 
 """
 This module contains function that checks if the object
-pased is exactly an instance of the specified class
+passed is exactly an instance of the specified class
 """
 
 
 def is_same_class(obj, a_class):
-    """Checks if object is exactly an instance of specified
-    class
-    Args:
-        obj: The object to check
-        a_class: The specified class
-    Return:
-        True: If obj is exactly and instance
-        False: If otherwise
-    """
+    """Implements sorted printing for the built-in list class."""
+
     return type(obj) == a_class
