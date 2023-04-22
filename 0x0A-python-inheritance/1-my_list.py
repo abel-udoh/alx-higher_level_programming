1fa04056fdc175fad043275d28d73d17c6ba698b
abel-udoh <abeludoh8@gmail.com>
2023-04-22 10:16:55 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ passed is exactly an instance of the specified class
 """
 
 
-def is_same_class(obj, a_class):
+def MyList(list):
     """Implements sorted printing for the built-in list class."""
 
     return type(obj) == a_class
