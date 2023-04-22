8b1cba38ec0cbfb68ea0e1eebd7aff2c2ccaba2b
abel-udoh <abeludoh8@gmail.com>
2023-04-22 09:51:40 -0700

    first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module with a function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """This function reads a text file (UTF8) and prints it to stdout"""
+
+    With open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
