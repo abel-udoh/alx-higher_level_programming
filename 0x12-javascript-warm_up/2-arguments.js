36138f6e4c21a91df5a756da8f225f4570b7e35c
abel-udoh <abeludoh8@gmail.com>
2023-06-15 13:37:29 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //print a message according to the number of argument passed
-if (process.argv.length < 1){
+if (process.argv.length < 3){
 	console.log('No Argument');
-} else if (process.argv.length ===1);{
+} else if (process.argv.length === 3);{
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');}
