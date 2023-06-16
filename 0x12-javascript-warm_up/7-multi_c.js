97eb43733bf6dfde081942ee0868ffd75b4c61df
abel-udoh <abeludoh8@gmail.com>
2023-06-16 16:13:21 -0700

    task 6

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints “C is fun” multiple times
+let i = 0;
+const argum = process.argv;
+if (argum.length === 2) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < argum[2]) {
+    console.log('C is fun');
+    i++;
+  }
+}
