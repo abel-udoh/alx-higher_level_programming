41e1ba96c72128dfe62cdee26b199af96910f359
abel-udoh <abeludoh8@gmail.com>
2023-06-15 21:55:51 -0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 //print the first argument that is passed into it
-
-if (process.argv[2] === undefined) {
+if (!process.argv[2]) {
   console.log('No Argument');
 } else {
   console.log(process.argv[2]);
