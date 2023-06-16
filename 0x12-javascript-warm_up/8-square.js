acf803bd47f7d2cf8f1bf85dc9185bf706b3d956
abel-udoh <abeludoh8@gmail.com>
2023-06-16 16:44:55 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let times = parseInt(process.argv[2]);
+let row = 0;
+let column = 0;
+
+if (isNaN(times)) {
+  console.log('Missing size');
+} else {
+  for (row = 0; row < times; row++) {
+    let result = '';
+    for (column = 0; column < times; column++) {
+      result += 'X';
+    }
+    console.log(result);
+  }
+}
