b47b50f1e98819643fbc283058e0a35b9c22249d
abel-udoh <abeludoh8@gmail.com>
2023-06-16 16:29:05 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i = 0;
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i = 0; i < array.length; i++) {
-  console.log(array[i]);
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
