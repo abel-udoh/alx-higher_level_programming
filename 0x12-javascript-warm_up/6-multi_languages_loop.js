28c7c2786be87ae921964967f39798518e969b93
abel-udoh <abeludoh8@gmail.com>
2023-06-16 17:54:40 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const quotes = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (quotes[i]) {
-  console.log(quotes[i]);
-  i++;
+// script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
