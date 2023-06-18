405893c63173d1f77941cf3c76164a3d2baab55e
abel-udoh <abeludoh8@gmail.com>
2023-06-18 12:44:55 -0700

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+// the constructor takes in two arguments width and height
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
