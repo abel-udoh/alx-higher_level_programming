1b915df65e8114331c6623b01f062eb3c33edc28
abel-udoh <abeludoh8@gmail.com>
2023-07-02 00:44:04 -0700

    network 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Finds Peak values """
-
 
 def find_peak(list_of_integers):
-    """Find the peak"""
-    list_l = len(list_of_integers)
-    if list_l is 0:
+    """Finds peak
+    Args:
+        list_of_integers (list): list of integers
+    """
+    len_lst = len(list_of_integers)
+    if len_lst < 3:
         return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
+    peak = list_of_integers[1]
+    for i in range(1, len_lst):
+        if list_of_integers[i] >= peak:
+            peak = list_of_integers[i]
+    return peak
 
-
-""" binary search algorithim """
-
-
-def binary_search(a, lo, hi):
-    """Recursive binary search of the peak"""
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
