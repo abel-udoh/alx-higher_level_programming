0c0f5822d20a9bf8d9e02b47b13408427cd649a9
abel-udoh <abeludoh8@gmail.com>
2023-07-04 08:40:03 -0700

    task 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ script that fetches https://intranet.hbtn.io/status using requests """
+
+import requests
+
 if __name__ == '__main__':
-    import requests
 
     response = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
